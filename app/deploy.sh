dd6056e74f60b48781895dfed8679f1a7961b750
jmeraq <jmeraq@hotmail.com>
2019-06-09 19:56:11 -0500

    deployment jenkinsfile

--- app/deploy.sh
+++ app/deploy.sh
@@ -2,7 +2,7 @@
 input="app/ips.txt"
 while IFS= read -r line
 do
-  ssh -i "/home/jmeraq/.ssh/id_rsa" ubuntu@"$line" "sudo docker pull jmeraq/microservice-bp:$1"
-  ssh -i "/home/jmeraq/.ssh/id_rsa" ubuntu@"$line" "sudo docker rm -f microservice-bp"
-  ssh -i "/home/jmeraq/.ssh/id_rsa" ubuntu@"$line" "sudo docker run --name microservice-bp -p 8081:8081 -dti jmeraq/microservice-bp:$1"
+  ssh -i "/root/.ssh/id_rsa" ubuntu@"$line" "sudo docker pull jmeraq/microservice-bp:$1"
+  ssh -i "/root/.ssh/id_rsa" ubuntu@"$line" "sudo docker rm -f microservice-bp"
+  ssh -i "/root/.ssh/id_rsa" ubuntu@"$line" "sudo docker run --name microservice-bp -p 8081:8081 -dti jmeraq/microservice-bp:$1"
 done < "$input"
